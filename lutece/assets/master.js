70162346762aa32cd67ffa3aeca0b0c24f44c055
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-10-30 18:11:47 +0900

    Enter webpack building
    * Add gulp tasks
    * Add manifest.json support
    * Create assets directory

---
+++ lutece/assets/master.js
@@ -0,0 +1,4 @@
+'use strict';
+
+require('./css/reset.styl');
+require('./css/style.styl');
--- main.py
+++ main.py
@@ -1,12 +1,54 @@
+# encoding: utf-8
 u"""Lut\xe8ce application"""
 from __future__ import print_function
 from __future__ import unicode_literals
+import json
+from os import path
 
 from flask import Flask, render_template
 
 
+class AssetConfigurator(object):
+    """Configure class for asset files are built via webpack.
+
+    This provides `built_asset_file` method to get filename from
+    manifest.json.
+    """
+    def __init__(self, _manifest_json):  # (str) -> None
+        self._app = None
+        self._assets = {}
+        self._manifest_json = _manifest_json
+
+    def init_app(self, _app):  # -> None
+        """Loads manifest.json and appends a template global function."""
+        self._app = _app
+        self._load_manifest_json()
+
+        _app.add_template_global(self.built_asset_file)
+
+    def _load_manifest_json(self):  # () -> None
+        try:
+            with self._app.open_resource(self._manifest_json, 'r') as fh:
+                self._assets = json.load(fh)
+        except IOError:
+            pass
+
+    def built_asset_file(self, filepath):  # (str) -> str
+        """Returns built asset file name if it is in manifest.json."""
+        key = filepath.encode()
+        if key not in self._assets:
+            return filepath
+        return '{0}'.format(self._assets[key])
+
+
 # pylint: disable=invalid-name
+manifest_json = path.join(path.dirname(__file__), 'static/manifest.json')
+
 app = Flask(__name__, static_folder='static', static_url_path='')
+
+config = AssetConfigurator(manifest_json)
+config.init_app(app)
+# pylint: enable=invalid-name
 
 
 @app.route('/')
